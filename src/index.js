00f1016d07c40593a49cec07644a04e6834d4e0b
Marcos <marcosgramajo96@gmail.com>
2024-04-12 01:36:01 -0300

    add google analytics and tailwindcss

--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ import './index.css';
 import './Styles/Styles.css'
 import App from './App';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyALBbhfZc3lOLDU26gxPmiCoQCkPcNCZyk",
+  authDomain: "radiadoresgm-13a43.firebaseapp.com",
+  projectId: "radiadoresgm-13a43",
+  storageBucket: "radiadoresgm-13a43.appspot.com",
+  messagingSenderId: "179592283999",
+  appId: "1:179592283999:web:2d2dbc659d17ebd4c358ea",
+  measurementId: "G-DW7QVRBMR1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
