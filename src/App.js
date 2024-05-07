cd39a8ae138dfb1abb163fb479614c2be1ea0546
Marcos <marcosgramajo96@gmail.com>
2024-05-06 23:44:44 -0300

    update galery

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Home from './components/Home'
 import Services from './components/services'
 import About from './components/about'
 import './App.css';
-import Gallery from './components/gallery'
+// import Gallery from './components/gallery'
 import Contact from './components/contact'
 import Banner from './components/banner'
-
+import Album from './components/album'
 
 function App() {
   return (
@@ -15,7 +15,8 @@ function App() {
       <Services/>
       <About/>
       <Banner/>
-      <Gallery/>
+      {/* <Gallery/> */}
+      <Album/>
       <Contact/>
 
     </div>
---
+++ src/components/album.js
@@ -0,0 +1,97 @@
+
+export default function Gallery() {
+  return (
+    <div className="m-auto mb-6" style={{width:'100%', maxWidth:'1100px'}}>
+        <div className="aboutTitle">
+            NUESTROS TRABAJOS
+            </div>
+      <div class="grid grid-cols-2 gap-4 md:grid-cols-4">
+        <div class="grid gap-4">
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://res.cloudinary.com/dyjkycesi/image/upload/v1715048856/album/mpvgpcydwswztknssy4k.jpg"
+              alt="gallery"
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg object-cover object-center "
+              src="https://res.cloudinary.com/dyjkycesi/image/upload/v1715043908/album/xtugvcmxm57d4bdzfbhv.jpg"
+              alt="gallery"
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://res.cloudinary.com/dyjkycesi/image/upload/v1715049101/album/xvfljcnmcmbhvewc7x2y.jpg"
+              alt="gallery"
+            />
+          </div>
+        </div>
+        <div class="grid gap-4">
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://res.cloudinary.com/dyjkycesi/image/upload/v1715049193/album/wp9t7if1ltwj8grii5cx.jpg"
+              alt="gallery"
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://res.cloudinary.com/dyjkycesi/image/upload/v1715049194/album/qdd9vsfgwqat5sef4ipp.jpg"
+              alt="gallery"
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg object-cover object-center "
+              src="https://res.cloudinary.com/dyjkycesi/image/upload/v1715049195/album/v967p9jyxebym2sy5apf.jpg"
+              alt="gallery"
+            />
+          </div>
+        </div>
+        <div class="grid gap-4">
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://res.cloudinary.com/dyjkycesi/image/upload/v1715049441/album/b0xxxgrr19o4x1jvf0ft.jpg"
+              alt="gallery"
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg object-cover object-center "
+              src="https://res.cloudinary.com/dyjkycesi/image/upload/v1715049441/album/apmxo7ntg8mmpfkn1cuf.jpg"
+              alt="gallery"
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://res.cloudinary.com/dyjkycesi/image/upload/v1715049195/album/xesbrkeklxqmhkarw6li.jpg"
+              alt="gallery"
+            />
+          </div>
+        </div>
+        <div class="grid gap-4">
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://res.cloudinary.com/dyjkycesi/image/upload/v1715049617/album/cwdhbfwq1syav5ssvdut.jpg"
+              alt="gallery"
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg object-cover object-center"
+              src="https://res.cloudinary.com/dyjkycesi/image/upload/v1715049617/album/yggtmlvabrotxezscqez.jpg"
+              alt="gallery"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
